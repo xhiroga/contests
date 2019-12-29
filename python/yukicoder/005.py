7d57fe71534f8599b26d14dc3ee1914be8301689
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-12-29 09:42:32 +0900

    move dir

--- python/yukicoder/005.py
+++ python/yukicoder/005.py
@@ -1,13 +1,15 @@
 # coding: UTF-8
 
+
 def main():
-    num = int (input())
-    if (num % 15 == 0):
+    num = int(input())
+    if num % 15 == 0:
         print("FizzBuzz")
-    elif (num % 3 == 0):
-            print ("Buzz")
-    elif (num % 5 == 0):
-        print ("Fizz")
+    elif num % 3 == 0:
+        print("Buzz")
+    elif num % 5 == 0:
+        print("Fizz")
+
 
 if __name__ == "__main__":
     main()
