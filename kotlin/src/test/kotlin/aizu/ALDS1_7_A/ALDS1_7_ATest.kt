a8abbcafa6a605d8f814b46f572dd50438bd6311
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-01-05 05:44:50 +0900

    プログラミングコンテスト攻略のためのアルゴリズムとデータ構造 7 木 パッケージ整理

--- kotlin/src/test/kotlin/aizu/ALDS1_7_A/ALDS1_7_ATest.kt
+++ kotlin/src/test/kotlin/aizu/ALDS1_7_A/ALDS1_7_ATest.kt
@@ -8,17 +8,32 @@ class ALDS1_7_ATest {
     fun case1() {
         assertEquals(
             listOf(
-                TreeOutput(nodeId = 0, parent = -1, depth = 0, leaves = listOf(1, 4, 10)),
+                TreeOutput(
+                    nodeId = 0,
+                    parent = -1,
+                    depth = 0,
+                    leaves = listOf(1, 4, 10)
+                ),
                 TreeOutput(nodeId = 1, parent = 0, depth = 1, leaves = listOf(2, 3)),
                 TreeOutput(nodeId = 2, parent = 1, depth = 2, leaves = listOf()),
                 TreeOutput(nodeId = 3, parent = 1, depth = 2, leaves = listOf()),
-                TreeOutput(nodeId = 4, parent = 0, depth = 1, leaves = listOf(5, 6, 7)),
+                TreeOutput(
+                    nodeId = 4,
+                    parent = 0,
+                    depth = 1,
+                    leaves = listOf(5, 6, 7)
+                ),
                 TreeOutput(nodeId = 5, parent = 4, depth = 2, leaves = listOf()),
                 TreeOutput(nodeId = 6, parent = 4, depth = 2, leaves = listOf()),
                 TreeOutput(nodeId = 7, parent = 4, depth = 2, leaves = listOf(8, 9)),
                 TreeOutput(nodeId = 8, parent = 7, depth = 3, leaves = listOf()),
                 TreeOutput(nodeId = 9, parent = 7, depth = 3, leaves = listOf()),
-                TreeOutput(nodeId = 10, parent = 0, depth = 1, leaves = listOf(11, 12)),
+                TreeOutput(
+                    nodeId = 10,
+                    parent = 0,
+                    depth = 1,
+                    leaves = listOf(11, 12)
+                ),
                 TreeOutput(nodeId = 11, parent = 10, depth = 2, leaves = listOf()),
                 TreeOutput(nodeId = 12, parent = 10, depth = 2, leaves = listOf())
             ),
